a24021246e3e37b99fc8bf27860af8144a227952
Lionel Mason <capitalcookhouse@gmail.com>
2024-11-23 11:24:15 -0500

    completes parrot lab

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,5 @@
-def parrot():
+def parrot(greeting = "Squawk!"):
     pass
+    # accepts argument of a string and both prints and returns the string at the end of the function
+    print(greeting)
+    return greeting
